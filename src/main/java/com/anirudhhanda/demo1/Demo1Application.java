4a3f0f0c83557f3411c4e820bc9f5a009bb170d0
AnirudhHanda <anirudhhanda3@gmail.com>
2024-04-24 00:27:03 +0530

    added a Student class

--- src/main/java/com/anirudhhanda/demo1/Demo1Application.java
+++ src/main/java/com/anirudhhanda/demo1/Demo1Application.java
@@ -1,10 +1,13 @@
 package com.anirudhhanda.demo1;
 
+import com.anirudhhanda.demo1.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,14 @@ public class Demo1Application {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("hello", "homi", "how", "are", "you");
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L,
+						"Anirudh Handa",
+						20,
+						"anirudhhanda3@gmail.com",
+						LocalDate.of(2004, Month.APRIL, 11))
+		);
 	}
 
 }
