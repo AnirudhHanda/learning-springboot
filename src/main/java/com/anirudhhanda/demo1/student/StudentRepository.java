4820a318a1740dee81922ff418651f5b4cb9e572
AnirudhHanda <anirudhhanda3@gmail.com>
2024-04-25 07:28:38 +0530

    added spring data jpa and corrected dependency injection using autowired annotation and many more till connecting to the database and creating the service layer i.e a jpaRepository

---
+++ src/main/java/com/anirudhhanda/demo1/student/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.anirudhhanda.demo1.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
