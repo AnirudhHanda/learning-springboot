e47cd10667c069a381cf330d638a88cc22143bcd
AnirudhHanda <anirudhhanda3@gmail.com>
2024-04-25 08:36:58 +0530

    added two users to database

---
+++ src/main/java/com/anirudhhanda/demo1/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.anirudhhanda.demo1.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student anirudh = new Student(
+                    "Anirudh Handa",
+                    21,
+                    "anirudhhanda3@gmail.com",
+                    LocalDate.of(2004, Month.APRIL, 11)
+            );
+
+            Student aditi = new Student(
+                    "Aditi Handa",
+                    23,
+                    "aditihanda43@gmail.com",
+                    LocalDate.of(2002, Month.JULY, 8)
+            );
+
+            repository.saveAll(
+                    List.of(anirudh, aditi)
+            );
+        };
+    }
+}
